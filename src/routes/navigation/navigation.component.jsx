7ff6e971093a595130cde8a3df7341f88f229253
Cris <crisecheguren@gmail.com>
2023-02-09 12:17:57 -0500

    added nav bar routes and logo, messed with some styling

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,23 @@
+import { Fragment } from 'react';
+import { Link, Outlet } from 'react-router-dom';
+import './navigation.styles.scss';
+
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+
+const Navigation = () => {
+  return (
+    <Fragment>
+        <div className='navigation'>
+        <Link className='logo-container' to='/'>
+            <Logo className='logo' />
+        </Link>
+            <div className='nav-links-container'>
+                <Link className='nav-link' to='/shop'>Shop</Link>
+            </div>
+        </div>
+        <Outlet />
+    </Fragment>
+  );
+}
+
+export default Navigation;
