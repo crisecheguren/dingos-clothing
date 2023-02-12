5fa2becf4c8aeb6d6dd703aa2738ce32e10ec818
Cris <crisecheguren@gmail.com>
2023-02-11 21:47:03 -0500

    added firebase and google signin

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,26 @@
+import { initializeApp } from 'firebase/app';
+import { 
+        getAuth, 
+        signInWithRedirect,
+        signInWithPopup,
+        GoogleAuthProvider,
+    } from "firebase/auth";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCFq3KQ2QhEn8xpSORXOdevmBVa7t-d6I4",
+    authDomain: "dingos-clothing.firebaseapp.com",
+    projectId: "dingos-clothing",
+    storageBucket: "dingos-clothing.appspot.com",
+    messagingSenderId: "242208921417",
+    appId: "1:242208921417:web:cb60e8e65445c88bc8ac98",
+    measurementId: "G-HWX25B4RJ1"
+  };
+  
+  // Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
