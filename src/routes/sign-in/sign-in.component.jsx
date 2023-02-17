782146ae6fe4078dad5cee6e0b0ae717d70976d1
Cris <crisecheguren@gmail.com>
2023-02-16 21:27:23 -0500

    messing with firebase

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,15 +1,25 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+import { 
+    signInWithGooglePopup, 
+    createUserDocumentFromAuth, 
+     
+} from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
+
+
     const logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user);
         createUserDocumentFromAuth(user);
     };
+
+
+
     return (
         <div>
             <h1>Sign In</h1>
             <button onClick={logGoogleUser}>Sign In With Google</button>
+
         </div>
     );
 };
