9d65fc636d7f48f3ae403be9249ce0a5dd131f3b
Cris <crisecheguren@gmail.com>
2023-03-05 09:50:44 -0500

    added checkout-item component and functionality

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,5 +1,6 @@
 import { useContext } from 'react';
 import { CartContext } from '../../contexts/cart.context';
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 
 import './checkout.styles.scss';
 
@@ -9,26 +10,33 @@ const Checkout = () => {
 
 
     return (
-       <div>
-            <h1>
-                Checkout
-            </h1>
-              <div>
+
+              <div className='checkout-container'>
+                <div className='checkout-header'>
+                    <div className='header-block'>
+                        <span>Product</span>
+                    </div>
+                    <div className='header-block'>
+                        <span>Description</span>
+                    </div>
+                    <div className='header-block'>
+                        <span>Quantity</span>
+                    </div>
+                    <div className='header-block'>
+                        <span>Price</span>
+                    </div>
+                    <div className='header-block'>
+                        <span>Remove</span>
+                    </div>
+
+                </div>
                 {
-                    cartItems.map((cartItem) => {
-                        const { name, price, quantity, id } = cartItem;
-                        return (
-                            <div key={id}>
-                                <h2>{name}</h2>
-                                <span>{quantity} x ${price}</span>
-                                <span onClick={() => removeItemFromCart(cartItem)}> decrement </span>
-                                <span onClick={() => addItemToCart(cartItem)}> increment</span>
-                            </div>
-                        );
-                    })
+                    cartItems.map((cartItem) => (
+                        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+                    ))
                 }
+                <span className='total'>Total: ${cartItems.reduce((total, cartItem) => total + cartItem.quantity * cartItem.price, 0)}</span>
             </div>
-         </div>
     );
 }
 
