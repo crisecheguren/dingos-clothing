4804e8a0de4a9a91c9eebc99eed4291f6b04974c
Cris <crisecheguren@gmail.com>
2023-04-12 16:06:23 -0400

    refactored my code to use reducer instead of state

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,5 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useEffect, useReducer } from "react";
+import { createAction } from "../utils/reducer/reducer.utils";
 import { onAuthStateChangedHandler, createUserDocumentFromAuth } from "../utils/firebase/firebase.utils";
 
 export const UserContext = createContext({
@@ -6,8 +7,38 @@ export const UserContext = createContext({
     setCurrentUser: () => null,
 });
 
+export const USER_ACTION_TYPES = {
+    SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+
+
+const userReducer = (state, action) => {
+    const { type, payload } = action;
+
+    switch (type) {
+        case USER_ACTION_TYPES.SET_CURRENT_USER:
+            return {
+                ...state,
+                currentUser: payload,
+            };
+        default:
+            throw new Error(`Unhandled action type: ${type}`);
+    }
+};
+
+const INITAL_STATE = {
+    currentUser: null,
+};
+
+
 export const UserProvider = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState(null);
+    const [state, dispatch] = useReducer(userReducer, INITAL_STATE);
+    const { currentUser } = state; 
+
+    const setCurrentUser = (user) => {
+        dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
+    };
+
     const value = { currentUser, setCurrentUser };
 
     useEffect(() => {
