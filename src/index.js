4c69554eb03641aab4d4f34afab1487f1b8bfc16
Cris <crisecheguren@gmail.com>
2023-04-15 15:17:04 -0400

    started migrating context to redux, converted user context and category stuff

--- src/index.js
+++ src/index.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-
+import { Provider } from 'react-redux';
 import App from './App';
-import { UserProvider } from './contexts/user.context';
-import { CategoriesProvider } from './contexts/categories.context';
+
 import { CartProvider } from './contexts/cart.context';
+import { store } from './store/store';
 import './index.scss';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>  
-      <UserProvider>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        
+          
+            <CartProvider>
+              <App />
+            </CartProvider>
+          
+        
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,7 +1,8 @@
 import { Fragment, useContext } from 'react';
 import { Outlet } from 'react-router-dom';
-import { UserContext } from '../../contexts/user.context';
 import { CartContext } from '../../contexts/cart.context';
+import { useSelector } from 'react-redux';
+import { selectCurrentUser } from '../../store/user/user.selector';
 import CartIcon from '../../components/cart-icon/cart-icon.component';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 import { signOutAuthUser } from '../../utils/firebase/firebase.utils';
@@ -12,8 +13,8 @@ import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 
 const Navigation = () => {
-  const { currentUser } = useContext(UserContext);
-  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+  const currentUser = useSelector(selectCurrentUser);
+  const { isCartOpen } = useContext(CartContext);
   
 
   
